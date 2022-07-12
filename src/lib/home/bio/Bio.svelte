0b0910a13ebe8bc958a5e0e65c918485473f83ac
Eugene Park <eugenepark2001@gmail.com>
2022-07-12 11:50:39 +0200

    Style the homepage

--- src/lib/home/bio/Bio.svelte
+++ src/lib/home/bio/Bio.svelte
@@ -1,15 +1,27 @@
-<div class="m-auto dark:text-[white]">
-	<h1>Hello, World! Thank you for visiting my portfolio. I hope you enjoy the stay!</h1>
-	<div class="flex gap-5 ">
-		<div class="">
-			<h2>Eugene Park</h2>
-			<h3>Student at University of Cologne</h3>
-			<h3>Information Systems B. Sc.</h3>
-		</div>
-		<img class="rounded w-52 h-44" src="/docs/images/profile.jpeg" alt="Profile picture" />
+<div class="m-auto flex flex-col gap-9 dark:text-[white]">
+	<div
+		class="block p-4  bg-slateGray rounded-3xl border border-blueMunsell shadow-md  dark:bg-blueMunsell dark:border-blueMunsell dark:hover:bg-gray-700"
+	>
+		<h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+			Hello, World!
+		</h5>
+		<p class="font-normal text-gray-700 dark:text-gray-400">
+			Thank you for visiting my portfolio page. I hope you enjoy the stay!
+		</p>
 	</div>
+	<div class="flex gap-5 ">
+		<div class="my-auto">
+			<h3 class="text-xl font-bold">Eugene Park</h3>
+			<p>Studying Information Systems B. Sc. at University of Cologne</p>
+		</div>
+		<img class="rounded-full w-32 h-28" src="/docs/images/profile.jpeg" alt="Profile picture" />
+	</div>
+	<div class="flex flex-col gap-2">
+		<h2 class="underline text-2xl font-bold">About Me</h2>
+	</div>
+
 	<div />
-	<div class=""><h2>Bio</h2></div>
-	<div class=""><h2>Hobbies</h2></div>
-	<div class=""><h2>Projects</h2></div>
+	<div class=""><h2 class="underline text-2xl font-bold">Education</h2></div>
+	<div class=""><h2 class="underline text-2xl font-bold">Hobbies</h2></div>
+	<div class=""><h2 class="underline text-2xl font-bold">Works</h2></div>
 </div>
